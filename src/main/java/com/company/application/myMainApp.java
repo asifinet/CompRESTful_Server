187cfb7e4ca7cf4dd3b08588442a206f0377e01e
Muhammad Asif <asif_inet@hotmail.com>
2016-03-11 12:15:29 +0800

    SpringBootable Main
    Added Spring bootable jars
    1.spring-boot-autoconfigure-1.2.3.RELEASE
    2.spring-boot-1.2.3.RELEASE

---
+++ src/main/java/com/company/application/myMainApp.java
@@ -0,0 +1,12 @@
+package com.company.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class myMainApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(myMainApp.class, args);
+    }
+}
